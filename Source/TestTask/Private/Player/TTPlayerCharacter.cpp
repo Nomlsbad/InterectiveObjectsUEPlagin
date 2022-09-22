e68bba4c6bf462b9eae6825dd9934121a46afa85
Nomlsbad <ilyayradima@mail.ru>
2022-09-22 18:20:30 +0300

    Add Player class

---
+++ Source/TestTask/Private/Player/TTPlayerCharacter.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/TTPlayerCharacter.h"
+
+ATTPlayerCharacter::ATTPlayerCharacter()
+{
+}
+
+void ATTPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
---
+++ Source/TestTask/Public/Player/TTPlayerCharacter.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/TestTaskCharacter.h"
+#include "TTPlayerCharacter.generated.h"
+
+UCLASS()
+class TESTTASK_API ATTPlayerCharacter : public ATestTaskCharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	ATTPlayerCharacter();
+
+protected:
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+};
