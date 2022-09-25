01ff24bf97171be8103e571a8a1b7843aae924d4
Nomlsbad <ilyayradima@mail.ru>
2022-09-25 17:25:12 +0300

    Add pressure plate class

---
+++ Source/TestTask/Public/InteractiveObjects/TTPressurePlate.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractiveObjects/TTBaseInteractiveActor.h"
+#include "TTPressurePlate.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTTASK_API ATTPressurePlate : public ATTBaseInteractiveActor
+{
+	GENERATED_BODY()
+	
+};
