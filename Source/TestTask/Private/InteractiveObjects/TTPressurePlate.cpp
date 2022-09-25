143a4f6096920503d15cc15ff3bf6d55e36e6ee2
Yurasov <iliayurasov@yandex.ru>
2022-09-25 22:17:48 +0300

    Create overlap events for pressure plate

--- Source/TestTask/Private/InteractiveObjects/TTPressurePlate.cpp
+++ Source/TestTask/Private/InteractiveObjects/TTPressurePlate.cpp
@@ -2,4 +2,54 @@
 
 
 #include "InteractiveObjects/TTPressurePlate.h"
+#include "Components/BoxComponent.h"
 
+ATTPressurePlate::ATTPressurePlate(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<UBoxComponent>(ATTBaseInteractiveActor::CollisionComponentName))
+{
+	Plate = CreateDefaultSubobject<UStaticMeshComponent>("Plate");
+	SetRootComponent(Plate);
+
+	CollisionComponent->SetupAttachment(Plate);
+	CollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &ATTPressurePlate::OnOverlapBegin);
+	CollisionComponent->OnComponentEndOverlap.AddDynamic(this, &ATTPressurePlate::OnOverlapEnd);
+}
+
+void ATTPressurePlate::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	CountOfOverlappingObjects += 1;
+	if (!bReadyToInteract) return;
+
+	bReadyToInteract = false;
+	StartInteraction();
+}
+
+void ATTPressurePlate::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	CountOfOverlappingObjects -= 1;
+	bReadyToInteract = CountOfOverlappingObjects == 0;
+
+	if (!bReadyToInteract) return;
+	
+	FinishInteraction();
+}
+
+void ATTPressurePlate::StartInteraction()
+{
+	
+}
+
+void ATTPressurePlate::FinishInteraction()
+{
+	
+}
+
+void ATTPressurePlate::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CountOfOverlappingObjects = 0;
+	bReadyToInteract = true;
+}
--- Source/TestTask/Public/InteractiveObjects/TTPressurePlate.h
+++ Source/TestTask/Public/InteractiveObjects/TTPressurePlate.h
@@ -6,12 +6,33 @@
 #include "InteractiveObjects/TTBaseInteractiveActor.h"
 #include "TTPressurePlate.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class TESTTASK_API ATTPressurePlate : public ATTBaseInteractiveActor
 {
 	GENERATED_BODY()
+
+public:
+
+	ATTPressurePlate(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 	
+	virtual void StartInteraction() override;
+	virtual void FinishInteraction() override;
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mesh")
+	UStaticMeshComponent* Plate;
+
+	int32 CountOfOverlappingObjects;
+
+	virtual void BeginPlay() override;
 };
