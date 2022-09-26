32820339e79c26796a1f2db5fb684628a3acbd7d
Yurasov Ilya <iliayurasov@yandex.ru>
2022-09-26 17:48:12 +0300

    Create lamp class

---
+++ Source/TestTask/Private/InteractiveObjects/TTLamp.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractiveObjects/TTLamp.h"
+
+ATTLamp::ATTLamp(const FObjectInitializer& ObjectInitializer)
+{
+}
---
+++ Source/TestTask/Public/InteractiveObjects/TTLamp.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractiveObjects/TTBaseInteractiveActor.h"
+#include "TTLamp.generated.h"
+
+
+UCLASS()
+class TESTTASK_API ATTLamp : public ATTBaseInteractiveActor
+{
+	GENERATED_BODY()
+
+public:
+	
+	ATTLamp(const FObjectInitializer& ObjectInitializer);
+	
+};
