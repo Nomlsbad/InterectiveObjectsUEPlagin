63f2787542213c46def2891683e05d2ab696e419
Yurasov Ilya <iliayurasov@yandex.ru>
2022-09-26 20:23:58 +0300

    Create TTPlayerInputInteractiveActor

---
+++ Source/TestTask/Public/InteractiveObjects/TTPlayerInputInteractiveActor.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractiveObjects/TTBaseInteractiveActor.h"
+#include "TTPlayerInputInteractiveActor.generated.h"
+
+
+UCLASS()
+class TESTTASK_API ATTPlayerInputInteractiveActor : public ATTBaseInteractiveActor
+{
+	GENERATED_BODY()
+	
+};
