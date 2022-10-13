a5cbdcd3f0fc003e64171b9b9c914f6608d96854
Yurasov Ilya <iliayurasov@yandex.ru>
2022-10-14 02:11:28 +0300

    Add hud

---
+++ Source/TestTask/Private/UI/TTGameHUD.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/TTGameHUD.h"
+#include "Blueprint/UserWidget.h"
+
+
+void ATTGameHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PlayerHUDWidget = CreateWidget<UUserWidget>(GetWorld(), PlayerHUDWidgetClass);
+	PlayerHUDWidget->AddToViewport();
+}
